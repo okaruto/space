5d1de9d499854cc71cdc09891343aa836c7d3d92
Okaruto Shirukoto <okaruto@protonmail.com>
2019-06-20 12:02:04 +0200

    Add default config.php for testing

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,3 +3,7 @@
 declare(strict_types=1);
 
 define('APPLICATION', __DIR__ . '/..');
+
+if (!file_exists(APPLICATION . '/data/config.php')) {
+    copy(APPLICATION . '/data/config.template.php', APPLICATION . '/data/config.php');
+}
